4fef27b4501cb231e4029105dbbde9c8cf808c9d
WSM18I8 <abbotttylerb@johndeere.com>
2022-08-13 16:10:36 -0500

    puzzle 83 wip

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Appearance, StyleSheet, Text, View} from 'react-native';
+import {useEffect, useState} from "react";
 
 export default function App() {
+  const [solved, setSolved] = useState(false)
+  const [colorSchemeHasChanged, setColorSchemeHasChanged] = useState(false)
+
+  useEffect(() => {
+    const originalColorScheme = Appearance.getColorScheme();
+
+    Appearance.addChangeListener(() => {
+      if (Appearance.getColorScheme() !== originalColorScheme) {
+        setColorSchemeHasChanged(true);
+      } else {
+        setSolved(true);
+      }
+    });
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{colorSchemeHasChanged  && !solved ? ' almost there!' : ''} {solved ? 'nice!' : 'good luck!'}</Text>
       <StatusBar style="auto" />
     </View>
   );
