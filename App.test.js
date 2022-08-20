a9441dc4072dfb77077ee188dc3740529909671c
Tyler Abbott <tylerabbott@Tylers-MacBook-Pro.local>
2022-08-20 15:53:19 -0500

    adding unit testing

--- App.test.js
+++ App.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import {render, screen, fireEvent} from '@testing-library/react-native';
 
 import App from './App';
 
 describe('<App />', () => {
-    it('has 1 child', () => {
-        const tree = renderer.create(<App />).toJSON();
-        expect(tree.children.length).toBe(1);
+    it('has 1 child', async () => {
+        render(<App />);
+        const button = await screen.findByText('good luck!');
     });
 });
