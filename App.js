0e7ec31d34545688c893f66945cc68e0818c3502
Tyler Abbott <tylerabbott@Tylers-MacBook-Pro.local>
2022-08-23 21:19:49 -0500

    add nativation

--- App.js
+++ App.js
@@ -1,8 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import {Appearance, StyleSheet, Text, View} from 'react-native';
+import {Appearance, StyleSheet, Text, View, Button} from 'react-native';
 import {useEffect, useState} from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="PuzzleOneScreen" component={PuzzleOneScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <Button
+      title="Go to first puzzle"
+      onPress={() =>
+        navigation.navigate('PuzzleOneScreen', { name: '' })
+      }
+    />
+  );
+};
+const PuzzleOneScreen = ({ navigation, route }) => {
   const [solved, setSolved] = useState(false);
   const [colorSchemeHasChanged, setColorSchemeHasChanged] = useState(false);
 
@@ -17,20 +52,10 @@ export default function App() {
       }
     });
   }, []);
-
   return (
-    <View style={styles.container}>
-      <Text>{colorSchemeHasChanged  && !solved ? ' almost there!' : ''} {solved ? 'nice!' : 'good luck!'}</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  <View style={styles.container}>
+    <Text>{colorSchemeHasChanged  && !solved ? ' almost there!' : ''} {solved ? 'nice!' : 'good luck!'}</Text>
+    <StatusBar style="auto" />
+  </View>
+  )
+};
