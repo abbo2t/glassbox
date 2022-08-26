cfe01a396ba521fe6dedbbe3f1d43e4b71214464
WSM18I8 <abbotttylerb@johndeere.com>
2022-08-26 17:42:36 -0500

    unit testing wip

--- tests/PuzzleOne.test.js
+++ tests/PuzzleOne.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import {render, screen, fireEvent} from '@testing-library/react-native';
 import PuzzleOne from '../components/PuzzleOne';
+import { Appearance } from 'react-native';
+
+jest.mock('Appearance', () => ({
+    getColorScheme: jest.fn(),
+    addChangeListener: jest.fn()
+}));
 
 describe('<PuzzleOne />', () => {
     it('has 1 child', async () => {
+        //Appearance.getColorScheme.mockReturnValueOnce('dark');
         render(<PuzzleOne />);
         const button = await screen.findByText('good luck!');
     });
